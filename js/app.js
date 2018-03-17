d1274cadef4920e96882d5b7a6cec37552aaf6c4
shaod23 <jakubnowakowski92@gmail.com>
2018-03-17 15:08:10 +0100

    eventy

---
+++ js/app.js
@@ -0,0 +1,22 @@
+    'use strict';
+
+    document.addEventListener('DOMContentLoaded', function () {
+        var showMenuDOM = document.querySelector('.for-dropdown');
+        var menuDOM = document.querySelector('.dropdown');
+        console.log(showMenuDOM, menuDOM);
+
+        showMenuDOM.addEventListener('mouseover', function () {
+            menuDOM.style.display = 'block'
+        });
+
+        showMenuDOM.addEventListener('mouseout', function () {
+            menuDOM.style.display = 'none'
+        });
+
+        // tutaj będziemy umieszczać kod ze wszystkich zadań
+    });
+
+
+    document.addEventListener('DOMContentLoaded', function () {
+
+    });
